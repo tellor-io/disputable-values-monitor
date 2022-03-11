64d3dbb46d6e1923c87c6f1f53fcbdb491cac272
oraclown <owen.burton13@gmail.com>
2022-03-11 14:38:41 -0500

    Parse event log into AttributeDict

--- tellor_disputables/data.py
+++ tellor_disputables/data.py
@@ -6,7 +6,8 @@ from web3 import Web3
 import os
 from telliot_core.directory import contract_directory
 import asyncio
-
+from web3.datastructures import AttributeDict
+from hexbytes import HexBytes
 
 
 def get_infura_node_url(chain_id: int) -> str:
@@ -26,11 +27,11 @@ def get_contract_info(chain_id):
     abi = contract_info.get_abi(chain_id=chain_id)
     return addr, abi
 
-def get_contract(chain_id: int):
+def get_web3(chain_id: int):
     node_url = get_infura_node_url(chain_id)
-    web3 = Web3(Web3.HTTPProvider(node_url))
-    addr, abi = get_contract_info(chain_id)
+    return Web3(Web3.HTTPProvider(node_url))
 
+def get_contract(web3, addr, abi):
     return web3.eth.contract(
         address=addr,
         abi=abi,
@@ -39,44 +40,48 @@ def get_contract(chain_id: int):
 
 # define function to handle events and print to the console
 def handle_event(event):
-    print(Web3.toJSON(event))
-    # print(event)
+    # print(Web3.toJSON(event))
+    print(event)
     # and whatever
 
 
 # asynchronous defined function to loop
 # this loop sets up an event filter and is looking for new entires for the "PairCreated" event
 # this loop runs on a poll interval
-async def eth_log_loop(event_filter, poll_interval):
-    while True:
-        for new_report in event_filter.get_new_entries():
-            handle_event(new_report)
-        await asyncio.sleep(poll_interval)
+async def eth_log_loop(event_filter, poll_interval, chain_id):
+    # while True:
+    unique_events = {}
+    unique_events_lis = []
+    for event in event_filter.get_new_entries():
+        txhash = event["transactionHash"]
+        if txhash not in unique_events:
+            unique_events[txhash] = event
+            unique_events_lis.append((chain_id, event))
+        # await asyncio.sleep(poll_interval)
+    return unique_events_lis
 
 
-async def poly_log_loop(event_filter, poll_interval, chain_id, loop_name):
-    node_url = get_infura_node_url(chain_id)
-    web3 = Web3(Web3.HTTPProvider(node_url))
+async def poly_log_loop(event_filter, poll_interval, chain_id, loop_name, web3, addr):
+    # while True:
+    num = web3.eth.get_block_number()
+    events = web3.eth.get_logs({
+        'fromBlock':num,
+        'toBlock': num+100,
+        'address':addr
+    })
 
-    addr, _ = get_contract_info(chain_id)
-
-    while True:
-        num = web3.eth.get_block_number()
-        events = web3.eth.get_logs({
-            'fromBlock':num,
-            'toBlock': num+100,
-            'address':addr
-        })
-
-        unique_events = {}
-        for event in events:
-            txhash = event["transactionHash"]
-            if txhash not in unique_events:
-                unique_events[txhash] = event
-                print('LOOP NAME:', loop_name)
-                handle_event(event)
-
-        await asyncio.sleep(poll_interval)
+    unique_events = {}
+    unique_events_lis = []
+    for event in events:
+        txhash = event["transactionHash"]
+        if txhash not in unique_events:
+            unique_events[txhash] = event
+            unique_events_lis.append((chain_id, event))
+            # print('LOOP NAME:', loop_name)
+            # handle_event(event)
+        # await asyncio.sleep(poll_interval)
+    
+    return unique_events_lis
 
 
 def get_new_reports():
@@ -116,36 +121,61 @@ def get_new_report(event_json: str):
         query_type=event["query_type"],
     )
 
+
 def create_eth_event_filter(chain_id):
     contract = get_contract(chain_id=chain_id)
     return contract.events.NewReport.createFilter(fromBlock='latest')
+
 
 def create_polygon_event_filter(chain_id):
     return None
 
 
-# when main is called
-# create a filter for the latest block and look for the "PairCreated" event for the uniswap factory contract
-# run an async loop
-# try to run the log_loop function above every 2 seconds
-def main():
+async def get_events(eth_web3, eth_oracle_addr, poly_web3, poly_oracle_addr):
     eth_mainnet_filter = create_eth_event_filter(1)
     eth_testnet_filter = create_eth_event_filter(4)
     polygon_mainnet_filter = create_polygon_event_filter(137)
     polygon_testnet_filter = create_polygon_event_filter(80001)
 
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_until_complete(
-            asyncio.gather(
-                eth_log_loop(eth_mainnet_filter, 2),
-                eth_log_loop(eth_testnet_filter, 2),
-                poly_log_loop(polygon_mainnet_filter, 2, 137, "tammy"),
-                poly_log_loop(polygon_testnet_filter, 2, 80001, "bob"),
-            ))
-    finally:
-        # close loop to free up system resources
-        loop.close()
+    events_lists = await asyncio.gather(
+                eth_log_loop(eth_mainnet_filter, 1, chain_id=1),
+                # eth_log_loop(eth_testnet_filter, 2),
+                # poly_log_loop(polygon_mainnet_filter, 2, 137, "tammy"),
+                poly_log_loop(polygon_testnet_filter, 1, 80001, "bob"),
+    )
+    return events_lists
+
+
+def parse_new_report_event(event, web3, contract):
+    # d = AttributeDict({
+    #     'address': '0x41b66dd93b03e89D29114a7613A6f9f0d4F40178',
+    #     'blockHash': HexBytes('0xa35a46281697bc9ddd4f19e2c2ea3d84ba2e7f9449a3aaae083e936b7f01265e'),
+    #     'blockNumber': 25471196,
+    #     'data': '0x000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000622b8b5400000000000000000000000000000000000000000000000000000000000000c000000000000000000000000000000000000000000000000000000000000000590000000000000000000000000000000000000000000000000000000000000100000000000000000000000000d5f1cc896542c111c7aa7d7fae2c3d654f34b92700000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000908625e1000000000000000000000000000000000000000000000000000000000000000c000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000d4c6567616379526571756573740000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000002',
+    #     'logIndex': 103,
+    #     'removed': False,
+    #     'topics': [HexBytes('0x48e9e2c732ba278de6ac88a3a57a5c5ba13d3d8370e709b3b98333a57876ca95')],
+    #     'transactionHash': HexBytes('0x47b3add1a2492760675aebcf0e4c4f953265dee0fc52fd608d12e827138ff948'),
+    #     'transactionIndex': 2})
+    # addr, abi = get_contract_info(80001)
+    # web3 = get_web3(80001)
+    # contract = get_contract(web3, addr, abi)
+    # x = contract.events.NewReport.processLog(d)
+    tx_hash = event['transactionHash']
+    receipt = web3.eth.getTransactionReceipt(tx_hash)
+    args = contract.events.NewReport().processReceipt(receipt)["args"]
+    query_id = args["_queryId"].decode('utf8')
+    timestamp = args["_time"]
+    value = args["_value"].decode('utf8')
+
+    
+    print(type(x))
+    print(x)
+
+
+def main():
+    # _ = asyncio.run(get_events())
+    test_parse_new_report()
 
 
 if __name__ == "__main__":
