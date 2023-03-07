68537736b6e5a9c458c8de6349139a233aa30b4b
tallywiesenberg <t.j.wies@gmail.com>
2023-03-07 05:38:51 -0500

    test get_contract

--- src/tellor_disputables/disputer.py
+++ src/tellor_disputables/disputer.py
@@ -1,114 +1,57 @@
 """Utilities for the auto-disputer on Tellor on any EVM network"""
 import logging
 from dataclasses import dataclass
-from enum import Enum
-from typing import Any
-from typing import Optional
-from typing import Union
-
-from telliot_core.model.base import Base
-from telliot_feeds.datafeed import DataFeed
+from chained_accounts import ChainedAccount
 
 
-class Metrics(Enum):
-    Percentage = "percentage"
-    Equality = "equality"
-    Range = "range"
+from telliot_core.apps.telliot_config import TelliotConfig
 
 
-@dataclass
-class Threshold(Base):
-    """
-    A Threshold for sending a dispute.
+from tellor_disputables.data import get_contract
+from tellor_disputables.utils import NewReport
 
-    amount (Optional[int]) -- amount of tolerated difference between
-    submitted on-chain values and trusted values from telliot.
+async def dispute(cfg: TelliotConfig, account: ChainedAccount, new_report: NewReport) -> None:
+    """Main dispute logic for auto-disputer"""
 
-    metric (Metrics) -- type of threshold
+    cfg.main.chain_id = new_report.chain_id
 
-    If self.metric == "percentage", amount is a percent with a minimum of 0
-    If self.metric == "equality", amount is None
-    If self.metric == "range", amount is the maximum distance an on-chain value can have from
-    the trusted value from telliot
-    """
+    token = get_contract(cfg, name="trb-token") #TODO test
+    governance = get_contract(cfg, name="tellor-governance")
 
-    metric: Metrics
-    amount: Union[int, float, None]
-
-    def __post_init__(self) -> None:
-
-        if self.metric == Metrics.Equality:
-            logging.warning("Equality threshold selected, ignoring amount")
-            self.amount = None
-
-        if self.metric != Metrics.Equality:
-            if self.amount is None:
-                raise ValueError(f"{self.metric} threshold selected, amount cannot be None")
-
-            if self.amount < 0:
-                raise ValueError(f"{self.metric} threshold selected, amount cannot be negative")
+    if token is None:
+        logging.log("Unable to find token contract")
+        return None
+    
+    if governance is None:
+        logging.log("Unable to find governance contract")
+        return None
 
 
-@dataclass
-class MonitoredFeed(Base):
-    feed: DataFeed[Any]
-    threshold: Threshold
-    query_id: Optional[str] = None
+    # read balance of user and log it
+    user_token_balance, status = await token.read(func_name="balanceOf", _address=account.address)
 
-    async def is_disputable(
-        self,
-        reported_val: Union[str, bytes, float, int, None],
-    ) -> Optional[bool]:
-        """Check if the reported value is disputable."""
-        if reported_val is None:
-            logging.error("Need reported value to check disputability")
-            return None
+    # read dispute fee and log it
+    dispute_fee, status = await governance.read(func_name="disputeFee")
 
-        trusted_val, _ = await general_fetch_new_datapoint(self.feed)
-        if isinstance(trusted_val, (str, int, float, bytes)):
+    # if balanceOf(user) < disputeFee, log "need more tokens to initiate dispute"
+    if user_token_balance < dispute_fee:
+        logging.info("User balance is below dispute fee: need more tokens to initiate dispute")
+        return None
 
-            if self.threshold.metric == Metrics.Percentage:
+    # write approve(governance contract, disputeFee) and log "token approved" if successful
+    tx_receipt, status = await token.write(func_name="approve", _spender=governance.address, _amount=dispute_fee)
 
-                if isinstance(trusted_val, (str, bytes)) or isinstance(reported_val, (str, bytes)):
-                    logging.error("Cannot evaluate percent difference on text/addresses/bytes")
-                    return None
-                if self.threshold.amount is None:
-                    logging.error("Please set a threshold amount to measure percent difference")
-                    return None
-                percent_diff: float = (reported_val - trusted_val) / trusted_val
-                return float(abs(percent_diff)) >= self.threshold.amount
+    if not status.ok:
+        logging.error("unable to approve tokens for dispute fee:" + status.msg)
+        return None
 
-            elif self.threshold.metric == Metrics.Range:
+    logging.info("Approval Tx Receipt: ", tx_receipt)
 
-                if isinstance(trusted_val, (str, bytes)) or isinstance(reported_val, (str, bytes)):
-                    logging.error("Cannot evaluate range on text/addresses/bytes")
+    # Write beginDispute(queryId, timestamp) and log “chain id <chain id>: dispute #(number of disputeId) initiated on query <Decoded Query Type w/ Parameters> – <queryId> at timestamp <Timestamp>
+    tx_receipt, status = await governance.write(func_name="beginDispute", _queryId=new_report.query_id, _timestamp=new_report.submission_timestamp)
 
-                if self.threshold.amount is None:
-                    logging.error("Please set a threshold amount to measure range")
-                    return None
-                range_: float = abs(reported_val - trusted_val)
-                return range_ >= self.threshold.amount
+    if not status.ok:
+        logging.error(f"unable to begin dispute on {new_report.query_id} at submission timestamp {new_report.submission_timestamp}:" + status.msg)
+        return None
 
-            elif self.threshold.metric == Metrics.Equality:
-
-                # if we have two bytes strings (not raw bytes)
-                if (
-                    (isinstance(reported_val, str))
-                    and (isinstance(trusted_val, str))
-                    and reported_val.startswith("0x")
-                    and trusted_val.startswith("0x")
-                ):
-                    return trusted_val.lower() != reported_val.lower()
-                return trusted_val != reported_val
-
-            else:
-                logging.error("Attemping comparison with unknown threshold metric")
-                return None
-        else:
-            logging.error("Unable to fetch new datapoint from feed")
-            return None
-
-
-async def general_fetch_new_datapoint(feed: DataFeed) -> Optional[Any]:
-    """Fetch a new datapoint from a datafeed."""
-    return await feed.source.fetch_new_datapoint()
+    logging.info("Dispute Tx Receipt: ", tx_receipt)
