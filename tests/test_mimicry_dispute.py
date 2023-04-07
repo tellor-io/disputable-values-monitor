0810fd9fef183caa68da3dda70db46c6d0789bd3
akrem <akremstudy@gmail.com>
2023-04-06 20:34:22 -0400

    refactor test based on suggestions

--- tests/test_mimicry_dispute.py
+++ tests/test_mimicry_dispute.py
@@ -10,30 +10,32 @@ from tellor_disputables.config import AutoDisputerConfig
 from tellor_disputables.data import get_contract
 from tellor_disputables.data import parse_new_report_event
 
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 
-
-def increase_time(seconds):
+def increase_time(w3, seconds):
     w3.provider.make_request("evm_increaseTime", [seconds])
     w3.provider.make_request("evm_mine", [])
 
 
-def take_snapshot():
+def take_snapshot(w3):
     return w3.provider.make_request("evm_snapshot", [])
 
 
-def revert_to_snapshot(snapshot_id):
+def revert_to_snapshot(w3, snapshot_id):
     w3.provider.make_request("evm_revert", [snapshot_id])
 
 
 @pytest.mark.asyncio
 async def test_disputability_mimicry_nft_index_type(setup, disputer_account):
     # get a snapshot of chain to revert to
-    snapshot_id = take_snapshot()
-    cfg = setup
-    cfg.main.chain_id = 1
-    cfg.endpoints.endpoints[0].url = "http://127.0.0.1:8545"
 
+    cfg = setup
+
+    cfg.main.chain_id = 1
+    endpoint = cfg.endpoints.endpoints[0]
+    endpoint.url = "http://127.0.0.1:8545"
+    endpoint.connect()
+    w3 = endpoint.web3
+    snapshot_id = take_snapshot(w3)
     oracle = get_contract(cfg, name="tellor360-oracle", account=disputer_account)
     token = get_contract(cfg, name="trb-token", account=disputer_account)
 
@@ -68,7 +70,7 @@ async def test_disputability_mimicry_nft_index_type(setup, disputer_account):
 
     first_receipt = w3.eth.wait_for_transaction_receipt(submit_hash.hex())
     # bypass reporter lock
-    increase_time(86400)
+    increase_time(w3, 86400)
     bad_value = val + (val * 0.9)
     bad_val_encoded = mimicry_nft_market_index_usd_feed.query.value_type.encode(bad_value)
     second_submit_txn = tellorflex.functions.submitValue(
@@ -82,16 +84,22 @@ async def test_disputability_mimicry_nft_index_type(setup, disputer_account):
     second_submit_hash = w3.eth.send_transaction(second_submit_txn)
     second_receipt = w3.eth.wait_for_transaction_receipt(second_submit_hash.hex())
     # revert the chain
-    revert_to_snapshot(snapshot_id["result"])
+    revert_to_snapshot(w3, snapshot_id["result"])
     with patch(
         "telliot_feeds.sources.mimicry.nft_market_index.NFTGoSource.fetch_new_datapoint",
         AsyncMock(side_effect=lambda: (val, datetime_now_utc())),
     ):
         parse_first_event = await parse_new_report_event(
-            cfg, monitored_feeds=AutoDisputerConfig().monitored_feeds, log=first_receipt["logs"][1]
+            cfg,
+            monitored_feeds=AutoDisputerConfig().monitored_feeds,
+            confidence_threshold=0.75,
+            log=first_receipt["logs"][1],
         )
         parse_second_event = await parse_new_report_event(
-            cfg, monitored_feeds=AutoDisputerConfig().monitored_feeds, log=second_receipt["logs"][1]
+            cfg,
+            monitored_feeds=AutoDisputerConfig().monitored_feeds,
+            confidence_threshold=0.75,
+            log=second_receipt["logs"][1],
         )
         # good report
         assert not parse_first_event.disputable
