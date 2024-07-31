57f7a5de8ba90a3ce0bce2d9f2a50eaf9fe6ac73
0xSpuddy <sloetterle@tellor.io>
2024-07-31 11:50:39 -0400

    new telliot releases

--- vars.example.sh
+++ vars.example.sh
@@ -1,4 +1,4 @@
-export MONITOR_NAME="DVM_1" #choose a name for your monitor
+export MONITOR_NAME="NAME_NOT_CONFIGURED" # please choose a name for your monitor
 export DISCORD_WEBHOOK_URL_1="https://discord.com/api/webhooks/your_webhook_url"
 export DISCORD_WEBHOOK_URL_2=""
 export DISCORD_WEBHOOK_URL_3=""
