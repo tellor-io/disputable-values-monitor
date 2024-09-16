33f78eb3586d0392593c640138e364c33fbfef24
Cjpotter10 <caleb10joseph@gmail.com>
2024-09-16 13:48:46 -0500

    added an api file to retrieve the csv files. Added flask to the pyproject file as it is a requirement of the api

---
+++ src/tellor_disputables/api.py
@@ -0,0 +1,22 @@
+from flask import Flask, send_file, abort
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Welcome to the Flask File API!'
+
+@app.route('/price-data/<string:id>/csv', methods=['GET'])
+def get_price_data_csv(id): 
+    csv_path = f'{id}_data.csv'
+    
+    # Check if the file exists
+    if not os.path.exists(csv_path):
+        abort(404, description=f'CSV file with ID {id} not found.')
+    
+    # Send the file
+    return send_file(csv_path, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
